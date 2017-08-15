d00b4405a56be7497223971e639a22c03839ca18
sy0317 <719711096@qq.com>
2017-08-15 19:58:18 +0800

    完成了实名认证的界面编码

---
+++ SijiuSdk7.3.3/src/main/java/com/shiyue/mhxy/user/Sy_ReadNameFragment_f.java
@@ -0,0 +1,20 @@
+package com.shiyue.mhxy.user;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+public class Sy_ReadNameFragment_f extends Fragment {
+
+
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+}
---
+++ SijiuSdk7.3.3/src/main/java/com/shiyue/mhxy/user/Sy_RealNameActivity.java
@@ -0,0 +1,16 @@
+package com.shiyue.mhxy.user;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.shiyue.mhxy.R;
+
+public class Sy_RealNameActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sy_real_name_activity);
+    }
+
+}
